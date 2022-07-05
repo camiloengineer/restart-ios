fec7783d73dd66333f7c46a5af47e16dff5b9870
Camilo González <cgonzalezm1234@gmail.com>
2022-07-05 07:34:38 -0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Camilo Gonzalez on 05-07-22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
