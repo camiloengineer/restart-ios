387cb57bf975583c8559c8e132b41fd8a6c30c7d
Camilo González <cgonzalezm1234@gmail.com>
2022-07-08 19:49:42 -0400

    first commit

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if isOnboardingViewActive {
+            OnboardingView()
+        } else {
+            HomeView()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13")
     }
 }
